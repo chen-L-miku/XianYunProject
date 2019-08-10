01f4cd2859688d800d2a27db92eeb4efed48225c
zhaolifeng <zhao@xxlifeng.com>
2019-08-10 16:39:36 +0800

    酒店首页

---
+++ components/hotel/hotelList.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="list">
+    <!-- 循环数组展示数据在页面 -->
+    <el-row
+      style="margin-top:20px;padding-bottom:10px;border-bottom:1px solid #ededed;"
+      v-for="(item,index) in data"
+      :key="index"
+    >
+      <el-col :span="8">
+        
+          <!-- 动态展示图片 -->
+          <img :src="item.photos" @click="getId(item.id)"/>
+        
+      </el-col>
+      <el-col :span="8" style="margin-left:30px;line-height:28px">
+        <div class="content">
+          <nuxt-link to="#" style="font-size:20px;color:black">{{item.name}}</nuxt-link>
+          <div>
+            <i style="color:#999">{{item.alias}}</i>
+            <i class="iconfont iconhuangguan" style="color:#f90"></i>
+            <i class="iconfont iconhuangguan" style="color:#f90" ></i>
+            <i class="iconfont iconhuangguan" style="color:#f90"></i>
+            {{item.hoteltype.name}}
+          </div>
+          <div>
+            <i class="el-rate__icon el-icon-star-on" style="color: rgb(247, 186, 42);"></i>
+            <i class="el-rate__icon el-icon-star-on" style="color: rgb(247, 186, 42);"></i>
+            <i class="el-rate__icon el-icon-star-on" style="color: rgb(247, 186, 42);"></i>
+            <i class="el-rate__icon el-icon-star-on" style="color: rgb(239, 242, 247);">
+              <i
+                class="el-rate__decimal el-icon-star-on"
+                style="color: rgb(247, 186, 42); width: 50%;"
+              ></i>
+            </i>
+            <i class="el-rate__icon el-icon-star-on"></i>
+            <span style="color:#f90">{{item.stars}}分</span>
+            <span style="font-size:18px;padding-left:10px">
+              <i style="color:#f90;font-size:30px;">36</i>条评价
+            </span>
+            <span style="font-size:18px; padding-left:10px">
+              <i style="color:#f90;font-size:30px;">76</i>篇游记
+            </span>
+          </div>
+          <div>
+            <span>
+              <i class="iconfont iconlocation"></i>
+              位于: {{item.address}}
+            </span>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="6" style="margin-left:30px;line-height:30px;">
+        <nuxt-link
+          to="#"
+          style="border-bottom:1px solid #ededed;padding-left:10px"
+          class="link"
+          v-for="(value,index) in item.products"
+          :key="index"
+        >
+          <span style>{{value.name}}</span>
+          <span style="text-align:right;display: inline-block; ">
+            <i style="color:#f90">￥{{value.price}}</i>
+            起>
+          </span>
+        </nuxt-link>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  // 接收父组件传过来的值
+  props: {
+    data: {
+      type: Array,
+      default:[]
+    }
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    getId(id){
+      console.log('id',id);
+      this.$router.push({
+        path:'/hotel/hotelDetails/'+id,
+      })
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.hotelList {
+  margin-top: 30px;
+  img {
+    display: inline-block;
+    width: 325px;
+    height: 250px;
+  }
+}
+a {
+  display: block;
+  line-height: 40px;
+}
+.link {
+  display: flex;
+  justify-content: space-between;
+  line-height: 60px;
+}
+/*清除浮动*/
+.clearfix::after {
+  visibility: hidden;
+  clear: both;
+  display: block;
+  content: ".";
+  height: 0;
+}
+</style>
---
+++ pages/hotel/index.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="hotel">
+    <!-- 面包屑 -->
+    <div class="head">
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }">酒店</el-breadcrumb-item>
+        <el-breadcrumb-item>{{val||'南京'}}市酒店预订</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <!-- input表单 -->
+    <div class="hotels">
+      <Hotels @getAddress="getAddress" />
+    </div>
+    <!-- 下拉列表 -->
+    <div class="select">
+      <Select @setSelect="setSelect " @gettion="gettion" @getLitie="getLitie" @gethotel="gethotel" />
+    </div>
+    <!-- 酒店列表 -->
+    <div class="hotelList">
+      <!-- 里面有几条数据子组件就会循环几次,就会展示几条 -->
+      <HotelList :data="hotelList" />
+    </div>
+    <div class="clearfix">
+      <!-- 分页 -->
+      <!-- @size-change：修改条数时候触发
+                    @current-change：修改页数时候触发
+                    current-page：当前页面
+                    page-size：当前页面显示的条数
+      total：总条数-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pageNum"
+        :page-sizes="[1, 2, 3, 4]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        style="float:right;margin-top:20px"
+      ></el-pagination>
+    </div>
+  </div>
+</template>
+<script>
+import Hotels from "@/components/hotel/hotels";
+import Select from "@/components/hotel/select";
+import HotelList from "@/components/hotel/hotelList";
+export default {
+  data() {
+    return {
+      pageNum: 2,
+      pageSize: 2,
+      total: 0,
+      val: "",
+      // 展示数据的数组
+      hotelList: [],
+      // 全部数据的数组
+      allHotelList: [],
+      allHotel: []
+    };
+  },
+  mounted() {
+    this.$axios({
+      url: "/hotels"
+    }).then(res => {
+      this.allHotelList = res.data.data;
+      // this.allHotel=res.data.data.slice(0)
+      this.allHotel = [...res.data.data];
+      this.total = this.allHotelList.length;
+      //  一开始默认展示条数
+      this.hotelList = this.allHotelList.slice(0, 2);
+      console.log("小酒店", this.allHotelList);
+    });
+  },
+  methods: {
+    // 酒店
+    gethotel(hotel) {
+      let sethotel = this.allHotel.filter(value => {
+        return value.name === hotel;
+      });
+      console.log("xiao", sethotel);
+      if (sethotel) {
+        this.pageNum = 1;
+        this.hotelList = sethotel;
+        this.total = sethotel.length;
+      }
+    },
+    // 设施
+    getLitie(arr) {
+      if (arr) {
+        this.pageNum = 1;
+        this.hotelList = arr;
+        this.total = arr.length;
+      }
+    },
+    // 类型
+    gettion(name) {
+      let hoteltype = [];
+      this.allHotel.forEach(function(item, index) {
+        if (item.hoteltype.name) {
+          hoteltype.push(item);
+        }
+      });
+      let allHoteltype = hoteltype.filter(item => {
+        return item.hoteltype.name === name;
+      });
+      console.log("第三次", allHoteltype);
+      if (allHoteltype) {
+        this.pageNum = 1;
+        this.hotelList = allHoteltype;
+        this.total = allHoteltype.length;
+      }
+    },
+
+    // 星级筛选
+    setSelect(str) {
+      let allstry = [];
+      this.allHotel.forEach(function(item, index) {
+        if (item.hotellevel) {
+          allstry.push(item);
+        }
+      });
+      let allstar = allstry.filter(item => {
+        return item.hotellevel.name === str;
+      });
+      if (allstar) {
+        this.pageNum = 1;
+        console.log("第三次", allstar);
+        this.hotelList = allstar;
+        this.total = allstar.length;
+      }
+    },
+    //   pageSize 改变时会触发
+    handleSizeChange(val) {
+      // console.log(`每页 ${val} 条`);
+      this.pageSize = val;
+      this.setList();
+    },
+    // currentPage 改变时会触发
+    handleCurrentChange(val) {
+      // console.log(`当前页: ${val}`);
+      this.pageNum = val;
+      this.setList();
+    },
+    // 接收子组件的数据
+    getAddress(add) {
+      this.val = add;
+    },
+    setTotal() {},
+    // 根据传进来的每页需要展示的条数或者当前码数来重新计算,提取到的值就作为展示的值
+    setList() {
+      this.hotelList = this.allHotelList.slice(
+        (this.pageNum - 1) * this.pageSize,
+        (this.pageNum - 1) * this.pageSize + this.pageSize
+      );
+    }
+  },
+  components: {
+    Hotels,
+    Select,
+    HotelList
+  }
+};
+</script>
+<style lang="less" scoped>
+.hotel {
+  width: 1000px;
+  margin: 0 auto;
+  color: #606266;
+  .head {
+    margin: 15px 0 15px 0;
+  }
+}
+/*清除浮动*/
+.clearfix::after {
+  visibility: hidden;
+  clear: both;
+  display: block;
+  content: ".";
+  height: 0;
+}
+</style>
